711c4ea626c6f1f0f5ee057a16e5259c318127c5
Babin007 <141405667+Babin007@users.noreply.github.com>
2026-02-20 14:58:38 +0000

    nav-bar update

--- inventory-app/src/app/app.routes.ts
+++ inventory-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'products',component:ProductsComponent},
+    {path:'contact',component:ContactComponent}
+];
